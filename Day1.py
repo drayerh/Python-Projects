bc80ce96636f705854f9293ef3ae8503645bec14
drayerh <107506795+drayerh@users.noreply.github.com>
2023-04-07 16:49:37 +0000

    Building a band name generator

---
+++ Day1.py
@@ -0,0 +1,8 @@
+#Go to: https://replit.com/@appbrewery/band-name-generator-start?v=1
+# Building my band name generator
+
+print("Welcome to my band name generator app.")
+city = input("Which city did you grow up in?\n")
+pet = input("What is the name of your pet?\n")
+band_name = (city + " " + pet)
+print(band_name)
